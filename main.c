455d2da36a648b6dd2c6f6b7ce1a87d79c61e2d4
Kevin Raoofi <nullindev@gmail.com>
2015-10-01 13:34:09 -0400

    Output more inline with requirements + more DRY
    * Utility function to write a matrix with label introduced
    * Prints to stdout written to look more like file writes
    * Time removed
    * No longer appends to output file

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "lib/matrix.h"
 #include "lib/sort.h"
 
 #define INPUT_FILE	"COSC450_P1_Data.txt"
 #define OUTPUT_FILE	"COSC450_P1_Output.txt"
 
+static void write_matrix(FILE* dest, const char *label, long int **matrix, int d1, int d2)
+{
+	fprintf(dest, "%s: \n", label);
+	arr_rect_join(dest, matrix, d1, d2, "", ", ", "\n\n");
+}
+
 int main()
 {
-
-	time_t t = time(0);
 
 	/** we can now read the file */
 	FILE *fin  = fopen(INPUT_FILE, "r");
@@ -25,42 +28,34 @@ int main()
 
 	int count = read_2_matrices(fin, &mat);
 
-	FILE *fout  = fopen(OUTPUT_FILE, "a");
+	FILE *fout;
 	
 	fclose(fin);
+
+	fout = fopen(OUTPUT_FILE, "w");
 	if(fout == NULL) {
 		perror(OUTPUT_FILE);
 		exit(-1);
 	}
 
-	printf("m1: \n");
-	arr_rect_join(stdout, mat[0], 5, count/10, "", ", ", "\n");
-	
-	printf("m2: \n");
-	arr_rect_join(stdout, mat[1], count/10, 5, "", ", ", "\n");
+	write_matrix(stdout, "Matrix 1", mat[0], 5, count/10);
+	write_matrix(fout, "Matrix 1", mat[0], 5, count/10);
+
+	write_matrix(stdout, "Matrix 2", mat[1], count/10, 5);
+	write_matrix(fout, "Matrix 2", mat[1], count/10, 5);
 
 	matrix_mult(mat[0], mat[1], &mult_mat, 5, count/10, 5);
-	printf("result: \n");
-	arr_rect_join(stdout, mult_mat, 5, 5, "", ", ", "\n");
 
-	fprintf(fout, "========== %s", ctime(&t));
-
-	fprintf(fout, "Multiplication Result: \n");
-	arr_rect_join(fout, mult_mat, 5, 5, "", ", ", "\n");
-
+	write_matrix(stdout, "Multiplication Result", mult_mat, 5, 5);
+	write_matrix(fout, "Multiplication Result", mult_mat, 5, 5);
 
 	sort(mult_mat, 5, 5);
 
-	fprintf(stdout, "Sorted Multiplication Result: \n");
-	arr_rect_join(stdout, mult_mat, 5, 5, "", ", ", "\n");
+	write_matrix(stdout, "Sorted Multiplication Result", mult_mat, 5, 5);
+	write_matrix(fout, "Sorted Multiplication Result", mult_mat, 5, 5);
 
-	fprintf(fout, "Sorted Multiplication Result: \n");
-	arr_rect_join(fout, mult_mat, 5, 5, "", ", ", "\n");
-
-	fprintf(fout, "==========\n\n");
-
-	printf("\nResults appended to: %s \n", OUTPUT_FILE);
 	fclose(fout);
+	printf("\nResults appended to: %s \n", OUTPUT_FILE);
 	
 	return 0;
 }
